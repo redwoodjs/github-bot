aee9d096c957ded33fd18022845903e3b1b902b9
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-06-25 01:50:24 +0900

    try being explicit about rootDir

--- api/jest.config.js
+++ api/jest.config.js
@@ -3,7 +3,7 @@
 const config = {
   rootDir: '../',
   preset: '@redwoodjs/testing/config/jest/api',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['./api/jest.setup.js'],
 }
 
 module.exports = config
