6b36aa63eecd4afcfd1bdbc494ae050e4d30cb50
Dominic Saadi <dominiceliassaadi@gmail.com>
2022-06-26 13:50:10 +0900

    fix: try fixing private key again

--- api/src/lib/github.ts
+++ api/src/lib/github.ts
@@ -1,3 +1,6 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
 import { createAppAuth } from '@octokit/auth-app'
 import { rest } from 'msw'
 import { Octokit } from 'octokit'
@@ -7,7 +10,14 @@ export const octokit = new Octokit({
   authStrategy: createAppAuth,
   auth: {
     appId: process.env.GITHUB_APP_ID,
-    privateKey: process.env.PRIVATE_KEY,
+    privateKey:
+      process.env.NODE_ENV === 'test'
+        ? // From https://github.com/probot/probot/blob/529197ed60ec4d978622882c0a0d843221ef9711/test/e2e/e2e.test.ts#L67-L82
+          '-----BEGIN RSA PRIVATE KEY-----\nMIIBOQIBAAJBAIILhiN9IFpaE0pUXsesuuoaj6eeDiAqCiE49WB1tMB8ZMhC37kY\nFl52NUYbUxb7JEf6pH5H9vqw1Wp69u78XeUCAwEAAQJAb88urnaXiXdmnIK71tuo\n/TyHBKt9I6Rhfzz0o9Gv7coL7a537FVDvV5UCARXHJMF41tKwj+zlt9EEUw7a1HY\nwQIhAL4F/VHWSPHeTgXYf4EaX2OlpSOk/n7lsFtL/6bWRzRVAiEArzJs2vopJitv\nA1yBjz3q2nX+zthk+GLXrJQkYOnIk1ECIHfeFV8TWm5gej1LxZquBTA5pINoqDVq\nNKZSuZEHqGEFAiB6EDrxkovq8SYGhIQsJeqkTMO8n94xhMRZlFmIQDokEQIgAq5U\nr1UQNnUExRh7ZT0kFbMfO9jKYZVlQdCL9Dn93vo=\n-----END RSA PRIVATE KEY-----'
+        : fs.readFileSync(
+            path.resolve(__dirname, '../../../private-key.pem'),
+            'utf-8'
+          ),
     installationId: process.env.GITHUB_INSTALLATION_ID,
   },
 })
